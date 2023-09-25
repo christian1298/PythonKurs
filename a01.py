812cf4f259be9617be46f58d9f78cf7d0a97ffa7
chrstian1298 <chrstian.meier48@gmail.com>
2023-09-25 10:34:19 +0200

    a5 & naming

--- a01.py
+++ a01.py
@@ -8,14 +8,14 @@ print("Aufgabe 1")
  "Drei-fach-Wert", False,
  "Füße_gemessen",True]
 
-3a = 1 
+#3a = 1 
 a = 1 ##
 Änderung = 1
 ÄNDERUNG = 1
-Mit,1 = 1
+#Mit,1 = 1
 Mit1 = 1 ##
-Label$3 = 1 ##
+#Label$3 = 1 ##
 Label3 = 1
-Drei-fach-Wert = 1
+#Drei-fach-Wert = 1
 Drei_fach_Wert = 1 ##
 Füße_gemessen=1
---
+++ a05.py
@@ -0,0 +1,5 @@
+a = 12
+b = 4
+txt = "a/dez=%d, b/dez=%d, a/hex=0x%x"
+
+print(str.format(txt % (a,b,a)))
