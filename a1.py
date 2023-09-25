e4460b05537b1362e10e58b2f1e3328d7fa51ce3
chrstian1298 <chrstian.meier48@gmail.com>
2023-09-25 10:12:01 +0200

    first commit

---
+++ a1.py
@@ -0,0 +1,21 @@
+print("Aufgabe 1")
+
+["3a", True,
+ "Änderung", True,
+ "ÄNDERUNG", True,
+ "Mit,1", False,
+ "Label$3", False,
+ "Drei-fach-Wert", False,
+ "Füße_gemessen",True]
+
+3a = 1 
+a = 1 ##
+Änderung = 1
+ÄNDERUNG = 1
+Mit,1 = 1
+Mit1 = 1 ##
+Label$3 = 1 ##
+Label3 = 1
+Drei-fach-Wert = 1
+Drei_fach_Wert = 1 ##
+Füße_gemessen=1
---
+++ a3.py
@@ -0,0 +1,10 @@
+wert_1 = "An" + "merkung"
+wert_2 = "-"*3 + " Lösung " + 3*"-"
+#wert_3 = kein_wert
+#wert_4 = 7*"7" + 8
+
+
+print(wert_1)
+print(wert_2)
+#print(wert_3)
+#print(wert_4)
