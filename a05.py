e7dc29a50a2ae88a519b7f56f16bf98336dccd12
chrstian1298 <chrstian.meier48@gmail.com>
2023-09-25 12:57:04 +0200

    07

--- a05.py
+++ a05.py
@@ -1,6 +1,6 @@
 a = 12
 b = 4
-txt = "a/dez={}, b/dez={}, a/hex=0x{:X}".format(a,b,a)
-print(txt)
+str = "a/dez={}, b/dez={}, a/hex=0x{:X}".format(a,b,a)
+print(str)
 txt2 = f"a/dez={a:d}, b/dez={b:d}, a/hex=0x{a:X}"
 print(txt2)
