699d1f5f8924d5aa5ec90e7a91585b0dcd0ed422
Paul Haworth <haworthpa74660@gmail.com>
2023-09-25 15:12:39 +0200

    andor

--- a13.py
+++ a13.py
@@ -1,9 +1,13 @@
 def my_and(a,b):
-    if (a & b): return True
-    return False
+    return a and b
 
 def my_or(a,b):
-    if(a | b): return True
-    return False
+    return a or b
 
-
+print("Adam" or False and "Eva")
+print(("Adam" or False) and "Eva")
+print("Adam" or (False and "Eva"))
+print(5*"-")
+print(my_or(my_and(False, "Eva"), "Adam"))
+print(my_and(my_or("Adam", False), "Eva"))
+print(my_or(my_and(False, "Eva"), "Adam"))
