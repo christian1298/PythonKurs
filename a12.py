22c5b1f903e14eacf99292281fd7918fbef40c1c
chrstian1298 <chrstian.meier48@gmail.com>
2023-09-25 14:43:56 +0200

    a12

---
+++ a12.py
@@ -0,0 +1,12 @@
+db = {
+    1000 : "Kartoffeln",
+    1020 : "Gurken",
+    720 : "Bananen",
+    702 : "Kiwis",
+    5000 : "Schokolade",
+    5010 : "Kartoffelchips"
+}
+
+while True:
+    t = input("Input: ")
+    print(db.get(int(t), "None"))
