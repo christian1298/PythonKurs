f123eca866cfa44b30990791948c86902379a305
Paul Haworth <haworthpa74660@gmail.com>
2023-09-26 14:43:39 +0200

    a24

---
+++ a24.py
@@ -0,0 +1,17 @@
+from a23_p import Rect
+with open("Dateien/Uebung/rects.txt", "r") as f:
+    l = f.readlines()
+
+r = []
+for i in l:
+    i = i.split(":")
+    try:
+        i[4] = i[4].split("\n")[0]
+    except IndexError:
+        exit()  
+    r.append(Rect(int(i[0]), int(i[1]), int(i[2]), int(i[3]), i[4]))
+
+tx = 60
+ty = 32
+for i in r:
+    i.contains(tx, ty)
