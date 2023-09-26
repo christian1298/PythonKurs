155286470105513a3eb8afaaf1dacc964a1caace
Paul Haworth <haworthpa74660@gmail.com>
2023-09-26 09:37:49 +0200

    a13

--- a13.py
+++ a13.py
@@ -1,8 +1,12 @@
 def my_and(a,b):
-    return a and b
+    if a and b: return b
+    else: return False
 
 def my_or(a,b):
-    return a or b
+    if a or b:
+        if a: return a
+        else: return b
+    else: return b
 
 print("Adam" or False and "Eva")
 print(("Adam" or False) and "Eva")
