e51c1fbc15ecfb615aa5c4ecb092812a411d1285
Paul Haworth <haworthpa74660@gmail.com>
2023-09-26 09:47:25 +0200

    a13 - no and/or

--- a13.py
+++ a13.py
@@ -8,10 +8,29 @@ def my_or(a,b):
         else: return b
     else: return b
 
+# print("Adam" or False and "Eva")
+# print(("Adam" or False) and "Eva")
+# print("Adam" or (False and "Eva"))
+# print(5*"-")
+# print(my_or(my_and(False, "Eva"), "Adam"))
+# print(my_and(my_or("Adam", False), "Eva"))
+# print(my_or(my_and(False, "Eva"), "Adam"))
+
+def my_and2(a,b):
+    if a:
+        if b: return b
+        else: return False
+    return False
+
+def my_or2(a,b):
+    if a: return a
+    return b
+
+
 print("Adam" or False and "Eva")
 print(("Adam" or False) and "Eva")
 print("Adam" or (False and "Eva"))
 print(5*"-")
-print(my_or(my_and(False, "Eva"), "Adam"))
-print(my_and(my_or("Adam", False), "Eva"))
-print(my_or(my_and(False, "Eva"), "Adam"))
+print(my_or2(my_and2(False, "Eva"), "Adam"))
+print(my_and2(my_or2("Adam", False), "Eva"))
+print(my_or2(my_and2(False, "Eva"), "Adam"))
