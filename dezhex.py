d7aad0e44e76dd62a3f28a1bf71b64c5a4d80b15
chrstian1298 <chrstian.meier48@gmail.com>
2023-09-28 12:19:19 +0200

    a29

--- dezhex.py
+++ dezhex.py
@@ -7,16 +7,26 @@ from PySide6.QtCore import Qt, QRect
 class Calc(QWidget):
     def __init__(self):
         super().__init__()
+        self.hex = None
+        self.dez = None
         self.initUI()
 
     def initUI(self):
         grid = QGridLayout()
         grid.addWidget(QLabel("Dezimal", self), 0,0)
-        grid.addWidget(QLineEdit("", self), 0,1)
+        self.dez = QLineEdit("", self)
+        grid.addWidget(self.dez, 0,1)
         grid.addWidget(QLabel("Hexal", self), 1,0)
-        grid.addWidget(QLineEdit("", self), 1,1)
+        self.hex =QLineEdit("", self)
+        self.hex.setReadOnly(True)
+        grid.addWidget(self.hex, 1,1)
         
-        grid.addWidget(QPushButton("Berechnung", self), 2,0,1,2)
+        b = QPushButton("Berechnung", self)
+        grid.addWidget(b, 2,0,1,2)
+        b.clicked.connect(self.buttonPressed)
 
         self.setLayout(grid)
         self.show()
+
+    def buttonPressed(self):
+        self.hex.setText(hex(int(self.dez.text())))
